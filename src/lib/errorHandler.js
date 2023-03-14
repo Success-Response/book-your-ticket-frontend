6b28504cf03f111e68fdfd97649a14cfbaac7600
John O’Sullivan <osullivanj.01@gmail.com>
2023-03-14 20:55:23 +0700

    adjust errorHandler

--- src/lib/errorHandler.js
+++ src/lib/errorHandler.js
@@ -1,16 +1,15 @@
 // eslint-disable-next-line consistent-return
 const errorHandler = (e, message = false) => {
   if (e instanceof TypeError) {
-    return `Something went wrong`;
+    return 'Something went wrong';
   }
 
   if (e instanceof Response) {
-    console.log('in errorHandler', e.status, e.statusText);
     switch (e.status) {
       case 404:
         return `${message} not found`;
       case 401:
-        return `Something went wrong while ${message}`;
+        return 'Something went wrong';
       default:
         break;
     }
