9ad8c6dd8a7dc9ea057f3e3a1ae06d768e1be6f3
John O’Sullivan <osullivanj.01@gmail.com>
2023-03-15 09:55:46 +0700

    Rework errorHandler

--- src/lib/errorHandler.js
+++ src/lib/errorHandler.js
@@ -1,24 +1,24 @@
-// eslint-disable-next-line consistent-return
-const errorHandler = (e, message = false) => {
-  let response = 'Something went wrong';
-  // FIXME
-  // if (e instanceof TypeError) {
-  //   return response;
-  // }
+// eslint-disable-next-line func-names
+export default function (e, message = false) {
+  const defaultResponse = 'Something went wrong';
+  let response = false;
+
+  if (e instanceof TypeError) {
+    response = defaultResponse;
+  }
 
   if (e instanceof Response) {
     switch (e.status) {
       case 404:
-        response = message ? `${message} not found` : response;
+        response = message ? `${message} not found` : defaultResponse;
         break;
       case 401:
         response = 'You are unable to access this resource';
         break;
       default:
+        response = defaultResponse;
         break;
     }
   }
   return response;
-};
-
-export default errorHandler;
+}
